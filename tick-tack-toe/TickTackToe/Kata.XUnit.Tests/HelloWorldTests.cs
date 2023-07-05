b8d09aaac75f9276ecdbbc21678333c70ff68b85
carles-soler-sunweb <carles.soler@sunwebgroup.com>
2023-07-05 15:03:30 +0200

    First test

--- tick-tack-toe/TickTackToe/Kata.XUnit.Tests/HelloWorldTests.cs
+++ tick-tack-toe/TickTackToe/Kata.XUnit.Tests/HelloWorldTests.cs
@@ -7,16 +7,10 @@ namespace Kata.XUnit.Tests
     public class HelloWorldTests
     {
         [Fact]
-        public void HelloWorld_Should_SayHelloWorld()
+        public void AnEmptyBoard_HasNineEmptyPositions()
         {
-            // Arrange
-            var sut = new HelloWorld();
-
-            // Act
-            var actual = sut.SayHelloWorld();
-
-            // Assert
-            actual.Should().Be("Oh oh ...");
+            Assert.Equals(board.positions, 9);
+            Assert.Equals(board.positions, 9);
         }
     }
 }
